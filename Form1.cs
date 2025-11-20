2db99001e6e6a755593283924daf9a480042765a
kalpajyoti <virmishra22@gmail.com>
2025-11-20 12:10:29 +0530

    Changes in form 1Initial commit

--- Form1.cs
+++ Form1.cs
@@ -24,7 +24,7 @@ namespace Vehicle_Activity_Tracking
             }
             catch (Exception ex)
             {
-               
+               // catch the exception
             }
             finally
             {
